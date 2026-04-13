8efccdacd081ebaf5727c48a8473c8a2b1b04144
southernleyte-source <southernleyte@psa.gov.ph>
2026-04-13 15:03:28 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Shared front-end configuration for the National ID booking system.
+// Replace this URL with your own Google Apps Script Web App URL.
+window.NID_API_CONFIG = Object.freeze({
+  EXEC_URL: "https://script.google.com/macros/s/AKfycbwX5tgcvZtl3BPQ7RtCPn6EoLKx2GBxCG6zEbUlWIsabKeJfEwFH5XusI6Vu8B145Wf3g/exec"
+});
